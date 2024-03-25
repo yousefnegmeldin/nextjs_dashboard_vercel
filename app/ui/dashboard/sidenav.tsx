69fd0a05210c90232acc917dddd4c56097c278bd
yousefnegm <yousefnegm@gmail.com>
2024-03-25 03:51:27 +0200

    added authentication

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-
+import { signOut } from '@/auth';
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
@@ -20,7 +20,15 @@ export default function SideNav() {
         <form>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
+            <div
+              className="hidden md:block"
+              action={async () => {
+                'use server';
+                await signOut();
+              }}
+            >
+              Sign Out
+            </div>
           </button>
         </form>
       </div>
